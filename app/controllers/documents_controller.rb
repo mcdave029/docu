1e0e78544db01360c800cda3b06f604c1ec6b1e8
Mc Dave Valdecantos <mdvaldecantos@gmail.com>
2018-02-10 15:43:10 +0800

    use strong parameters and add name as attribute sample for strong parameters

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -3,4 +3,20 @@ class DocumentsController < ApplicationController
     # declare a new object of document to be called in the new.html.erb
     @document = Document.new
   end
+
+  def create
+    if document = Document.new(document_params)
+      render plain: "Successfully created document"
+    else
+      render plain: "Failed to create document"
+    end
+  end
+
+  # whats the use of strong parameters strong paramteres act as the filter of your params
+  # on this part for example i have another attribute called file
+  # and file is not permitted as params of document even if file is pass
+  # as params it this part will stash the params and get only what is permitted
+  private def document_params
+    params.require(:document).permit(:name)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180210070401) do
+ActiveRecord::Schema.define(version: 20180210073823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180210070401) do
   create_table "documents", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "sections", force: :cascade do |t|
